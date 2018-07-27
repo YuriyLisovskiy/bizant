b95da972e599d67e6b18aadde79d8d5b5f3c14fe
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-07-28 02:34:36 +0300

    make all benchmarks constant size and multiple sizes

--- src/db/bucket_test.go
+++ src/db/bucket_test.go
@@ -1,3 +1,4 @@
+// Copyright (c) 2013 Ben Johnson
 // Copyright (c) 2018 Yuriy Lisovskiy
 // Distributed under the BSD 3-Clause software license, see the accompanying
 // file LICENSE or https://opensource.org/licenses/BSD-3-Clause.
