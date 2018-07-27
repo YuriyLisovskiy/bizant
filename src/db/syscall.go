41f2b1e2b1755566484d9441dcc3f4953695c13d
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-07-28 01:48:46 +0300

    refactor db commands into individual files, add DB.Check(), write freelist after each commit, remove DB.Open()

--- src/db/syscall.go
+++ src/db/syscall.go
@@ -2,6 +2,8 @@
 // Distributed under the BSD 3-Clause software license, see the accompanying
 // file LICENSE or https://opensource.org/licenses/BSD-3-Clause.
 
+// +build !linux
+
 package db
 
 import "os"
