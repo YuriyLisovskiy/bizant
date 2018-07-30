317908bba102772516372ccbe6eb37a2bb42db6d
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-07-30 12:31:59 +0300

    db: add caveat for memory usage, rework build support more liberal unix variants

--- src/db/boltsync_unix.go
+++ src/db/boltsync_unix.go
@@ -3,6 +3,8 @@
 // Distributed under the BSD 3-Clause software license, see the accompanying
 // file LICENSE or https://opensource.org/licenses/BSD-3-Clause.
 
+// +build !windows,!plan9,!linux
+
 package db
 
 import (
