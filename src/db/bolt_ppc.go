4d455ed55c91cc167e184e11c1d150f6f51d99e9
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-07-30 17:04:23 +0300

    db: add bolt_ppc.go to compile on 32-bit PPC platforms, move to separate lock file on windows, new bolt_ppc64.go similar to bolt_ppc64le.go, use tx.meta dureing Tx.WriteTo()

---
+++ src/db/bolt_ppc.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2013 Ben Johnson
+// Copyright (c) 2018 Yuriy Lisovskiy
+// Distributed under the BSD 3-Clause software license, see the accompanying
+// file LICENSE or https://opensource.org/licenses/BSD-3-Clause.
+
+// +build ppc
+
+package db
+
+// maxMapSize represents the largest mmap size supported by Bolt.
+const maxMapSize = 0x7FFFFFFF // 2GB
+
+// maxAllocSize is the size used when creating array pointers.
+const maxAllocSize = 0xFFFFFFF
