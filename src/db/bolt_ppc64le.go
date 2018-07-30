97aeacc35c24398c93d84f49a0ca303f7dc18f8b
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-07-30 13:45:19 +0300

    db: implement file locking for windows, add seperate files to compile on IBM Power and Z

--- src/db/bolt_ppc64le.go
+++ src/db/bolt_ppc64le.go
@@ -3,6 +3,8 @@
 // Distributed under the BSD 3-Clause software license, see the accompanying
 // file LICENSE or https://opensource.org/licenses/BSD-3-Clause.
 
+// +build ppc64le
+
 package db
 
 // maxMapSize represents the largest mmap size supported by Bolt.
