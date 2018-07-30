742e23e6c00494608a90f54f84797718b63123cc
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-07-30 15:28:52 +0300

    arm64 fix build

--- src/db/bolt_arm64.go
+++ src/db/bolt_arm64.go
@@ -3,9 +3,9 @@
 // Distributed under the BSD 3-Clause software license, see the accompanying
 // file LICENSE or https://opensource.org/licenses/BSD-3-Clause.
 
-package db
+// +build arm64
 
-package bolt
+package db
 
 // maxMapSize represents the largest mmap size supported by Bolt.
 const maxMapSize = 0xFFFFFFFFFFFF // 256TB
