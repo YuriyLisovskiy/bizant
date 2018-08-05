68c365941b2174e0705953b48e85e3d2e4149dd8
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-08-05 22:26:22 +0300

    update license notice of src/utils and src/wallet packages

--- src/utils/base58_test.go
+++ src/utils/base58_test.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2018 Yuriy Lisovskiy
-// Distributed under the BSD 3-Clause software license, see the accompanying
-// file LICENSE or https://opensource.org/licenses/BSD-3-Clause.
+// Distributed under the GNU General Public License v3.0 software license,
+// see the accompanying file LICENSE or https://opensource.org/licenses/GPL-3.0
 
 package utils
 
