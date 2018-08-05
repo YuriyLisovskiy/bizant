1756c93752990c41489e34ba72ee462546a23e7d
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-08-05 22:22:33 +0300

    update license notice of src/db and src/db/arch packages

--- src/db/arch/arch_ppc64le.go
+++ src/db/arch/arch_ppc64le.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2013 Ben Johnson
 // Copyright (c) 2018 Yuriy Lisovskiy
-// Distributed under the BSD 3-Clause software license, see the accompanying
-// file LICENSE or https://opensource.org/licenses/BSD-3-Clause.
+// Distributed under the GNU General Public License v3.0 software license,
+// see the accompanying file LICENSE or https://opensource.org/licenses/GPL-3.0
 
 // +build ppc64le
 
