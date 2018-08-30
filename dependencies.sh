2c9d157ec0f7ab7c48e82c0b6ee0a0a37574ef89
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-08-31 02:00:40 +0300

    crypto: add secp256k1 lib

--- dependencies.sh
+++ dependencies.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-if hash git 2>/dev/null
-then
-    echo Installing submodules...
-    git submodule init
-    git submodule update
-    echo Done.
-else
-    echo install.sh: can\'t install submodules$'\n'$'\t'reason: git is not installed, please install it and then try again
-fi
-
 if hash go 2>/dev/null
 then
     echo Downloading Golang required libraries...
